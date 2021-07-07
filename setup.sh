5ef22861bb55cbb4f23a2b9c8f4ae361097f06e8
Randy Fay <randy@randyfay.com>
2021-07-07 14:17:33 -0600

    Only define the hooks once in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Automatically configures mutagen sync for the current .ddev project.
+# Configures mutagen sync for the current ddev project.
 #
 
 echo "=> Checking ddev project"
@@ -26,42 +26,37 @@ echo "     https://raw.githubusercontent.com/cweagans/ddev-mutagen/master/mutage
 curl -s https://raw.githubusercontent.com/cweagans/ddev-mutagen/master/mutagen > .ddev/commands/host/mutagen
 chmod +x .ddev/commands/host/mutagen
 
-# If hooks are already present, let's not break their config.
-if cat .ddev/config.yaml | grep "^hooks:" > /dev/null; then
-  echo "=> It looks like you already have active hooks in your .ddev/config.yaml."
-  echo "   To finish setup, you'll need to add/merge the following config into"
-  echo "   what you already have configured:"
-  echo
-  echo
-  cat << EOF
+# These are the hooks we need to add
+hooks=$(cat <<EOF
 no_project_mount: true
 hooks:
   pre-start:
     # Make sure we don't already have a session running; it can confuse syncing
-    - exec-host: "mutagen sync terminate ${DDEV_PROJECT}"
-  post-start:
-    # Start the mutagen sync process for this project.
-    - exec-host: "ddev mutagen start"
-  pre-stop:
-    # Terminate the mutagen sync process for this project.
-    - exec-host: "ddev mutagen stop
-EOF
-  echo
-  echo
-# Otherwise, we can safely just tack this onto the end of the .ddev/config.yaml and
-# we're good to go.
-else
-  echo "=> Setting up .ddev/config.yaml to run the mutagen sync script"
-  cat >> .ddev/config.yaml <<'config'
-no_project_mount: true
-hooks:
+    - exec-host: "mutagen sync terminate \${DDEV_PROJECT}"
   post-start:
     # Start the mutagen sync process for this project.
     - exec-host: "ddev mutagen start"
   pre-stop:
     # Terminate the mutagen sync process for this project.
     - exec-host: "ddev mutagen stop"
-config
+EOF
+)
+
+# If hooks are already present, let's not break their config.
+if cat .ddev/config.yaml | grep "^hooks:" > /dev/null; then
+  echo "=> It looks like you already have active hooks in your .ddev/config.yaml."
+  echo "   To finish setup, you'll need to add/merge the following config into"
+  echo "   what you already have configured:"
+  echo
+  echo "$hooks"
+
+  echo
+  echo
+# Otherwise, we can safely just tack this onto the end of the .ddev/config.yaml and
+# we're good to go.
+else
+  echo "=> Setting up .ddev/config.yaml to run the mutagen sync script"
+  echo "$hooks" >> .ddev/config.yaml
 fi
 
 echo
